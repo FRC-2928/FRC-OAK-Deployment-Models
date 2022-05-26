13783261b9576b9e7d288fa8538401e5b813a9c9
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2022-05-25 18:59:04 -0700

    updates

--- img_helpers.py
+++ img_helpers.py
@@ -19,6 +19,7 @@ countFolder = 0
 count = 0
 imgList = []
 speedList = []
+rotateList = []
 
 #GET CURRENT DIRECTORY PATH
 myDirectory = os.path.join(os.getcwd(), 'DataCollected')
@@ -28,6 +29,7 @@ myDirectory = os.path.join(os.getcwd(), 'DataCollected')
 while os.path.exists(os.path.join(myDirectory,f'IMG{str(countFolder)}')):
         countFolder += 1
 newPath = myDirectory +"/IMG"+str(countFolder)
+newFilename = "DataCollected/IMG"+str(countFolder)
 os.makedirs(newPath)
 
 # SAVE IMAGES IN THE FOLDER
@@ -36,7 +38,8 @@ def saveData(img,speed, rotate):
     now = datetime.now()
     timestamp = str(datetime.timestamp(now)).replace('.', '')
     #print("timestamp =", timestamp)
-    fileName = os.path.join(newPath,f'Image_{timestamp}.jpg')
+    # fileName = os.path.join(newPath,f'Image_{timestamp}.jpg')
+    fileName = os.path.join(newFilename,f'Image_{timestamp}.jpg')
     cv2.imwrite(fileName, img)
     imgList.append(fileName)
     speedList.append(speed)
